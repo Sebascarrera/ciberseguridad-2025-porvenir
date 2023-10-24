22becb7e478da9fa1d3362860218382cb93659b5
sebascarreramoya <sebascarreramoya@gmail.com>
2023-10-24 10:08:47 -0500

    link transmision en vivo Ciberseguridad 24oct

--- src/Live/index.js
+++ src/Live/index.js
@@ -5,7 +5,7 @@ const LiveScreen = () => {
         <div className="container_live">
             <div className='container_live_frame'>
                 <iframe
-                    src="https://www.youtube.com/embed/r8BqNEBLoHI?si=vxBmusUdTRpHdJit" 
+                    src="https://www.youtube.com/embed/nF1cwf6n9B8?si=TyGQoszrTj6rblcp"
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
