ca19b23aa9aa213246d336de22af69c4d1ccc002
Felipe Carrera <pipecamo@gmail.com>
2023-10-24 08:09:07 -0500

    fixing url api

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://ciberseguridad-porvenir.uc.r.appspot.com'
+    baseURL: 'https://ciberseguridad-porvenir.uc.r.appspot.com'
 });
 
 export default instance 
