252ad5f76448b075c0940d0608dcbe96015d5018
Felipe Carrera <pipecamo@gmail.com>
2024-10-21 08:45:32 -0500

    cambio fuentes

--- src/Live/index.js
+++ src/Live/index.js
@@ -5,7 +5,7 @@ const LiveScreen = () => {
         <div className="container_live">
             <div className='container_live_frame'>
                 <iframe
-                    src="https://www.youtube.com/embed/Y98_gQKETUw?si=bxQoC23uL8uO5nX9"
+                    src="https://www.youtube.com/embed/f2fozUCBp88"
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
