3340f6326e4b34cd8592a9eac7468f24052746d2
Felipe Carrera <pipecamo@gmail.com>
2024-10-23 13:23:16 -0500

    cambio url live streaming

--- src/Live/index.js
+++ src/Live/index.js
@@ -5,7 +5,7 @@ const LiveScreen = () => {
         <div className="container_live">
             <div className='container_live_frame'>
                 <iframe
-                    src="https://www.youtube.com/embed/f2fozUCBp88"
+                    src="https://www.youtube.com/embed/UgyqDlVTyyo"
                     title="YouTube video player" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
